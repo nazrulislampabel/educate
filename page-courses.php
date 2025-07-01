6671f4a1e8c25efc8f7b5288937eba1f0ea0de1d
Nazrul Islam Pabel <independentnazrul@gmail.com>
2025-07-01 13:07:15 +0600

    Course Update

--- page-courses.php
+++ page-courses.php
@@ -12,7 +12,6 @@ $educate_course_arg = array(
 $educate_courses = new WP_Query($educate_course_arg);
 $courses_count = 0;
 ?>
-
 	<!-- Main content Start -->
 	<div class="main-content">
     <?php get_template_part("template-parts/common/breadcrumb")?>
@@ -20,154 +19,7 @@ $courses_count = 0;
 		<div class="rs-popular-courses style1 course-view-style orange-color rs-inner-blog white-bg pt-100 pb-100 md-pt-70 md-pb-70">
 			<div class="container">
 				<div class="row">
-					<div class="col-lg-4 col-md-12 order-last">
-						<div class="widget-area">
-							<div class="search-widget mb-50">
-								<h3 class="widget-title">Course Search</h3>
-								<div class="search-wrap">
-									<input type="search" placeholder="Searching..." name="s" class="search-input" value="">
-									<button type="submit" value="Search"><i class=" flaticon-search"></i></button>
-								</div>
-							</div>
-							<div class="widget-archives mb-50">
-								<h3 class="widget-title">Filter By</h3>
-								<div class="filter-widget">
-									<!-- Skills Form -->
-									<div class="filter-form">
-										<form method="post" action="mailer.php">
-											<div class="single-filter mb-30">
-												<h5>Skill Level</h5>
-												<!-- Radio Box -->
-												<div class="radio-box form-group">
-													<input type="radio" name="skill" id="type1" value="beginer" checked>
-													<label for="type1">Beginner</label>
-												</div>
-
-												<!-- Radio Box -->
-												<div class="radio-box form-group">
-													<input type="radio" name="skill" id="type2" value="intermediate">
-													<label for="type2">Intermediate</label>
-												</div>
-
-												<!-- Radio Box -->
-												<div class="radio-box form-group">
-													<input type="radio" name="skill" id="type3" value="expert">
-													<label for="type3">Expert</label>
-												</div>
-											</div>
-
-											<div class="single-filter mb-30">
-												<h5>Price Level</h5>
-												<!-- Radio Box -->
-												<div class="radio-box form-group">
-													<input type="radio" name="price" id="type4" value="free" checked>
-													<label for="type4">Free (14)</label>
-												</div>
-
-												<div class="radio-box form-group">
-													<input type="radio" name="price" id="type5" value="paid">
-													<label for="type5">Paid (26)</label>
-												</div>
-											</div>
-
-											<div class="single-filter mb-30">
-												<h5>Duration Time</h5>
-												<!-- Radio Box -->
-												<div class="radio-box form-group">
-													<input type="radio" name="duration" id="type6" value="six" checked>
-													<label for="type6">5+ hours (30)</label>
-												</div>
-
-												<div class="radio-box form-group">
-													<input type="radio" name="duration" id="type7" value="paid">
-													<label for="type7">10+ hours (20)</label>
-												</div>
-
-												<div class="radio-box form-group">
-													<input type="radio" name="duration" id="type8" value="paid">
-													<label for="type8">15+ hours (5)</label>
-												</div>
-											</div>
-
-											<div class="single-filter mb-30">
-												<h5>Course Type</h5>
-												<div class="check-box form-group mb-0">
-													<input type="checkbox" name="course" id="check1" value="check1" checked>
-													<label for="check1">Backend (3)</label>
-												</div>
-												<div class="check-box form-group mb-0">
-													<input type="checkbox" name="course" id="check2" value="check2">
-													<label for="check2">CSS (6)</label>
-												</div>
-												<div class="check-box form-group mb-0">
-													<input type="checkbox" name="course" id="check3" value="check3">
-													<label for="check3">Frontend (8)</label>
-												</div>
-												<div class="check-box form-group mb-0">
-													<input type="checkbox" name="course" id="check4" value="check4">
-													<label for="check4">General (3)</label>
-												</div>
-												<div class="check-box form-group mb-0">
-													<input type="checkbox" name="course" id="check5" value="check5">
-													<label for="check5">Photography (7)</label>
-												</div>
-												<div class="check-box form-group mb-0">
-													<input type="checkbox" name="course" id="check6" value="check6">
-													<label for="check6">Photography (4)</label>
-												</div>
-												<div class="check-box form-group mb-0">
-													<input type="checkbox" name="course" id="check7" value="check7">
-													<label for="check7">Technology (5)</label>
-												</div>
-											</div>
-
-											<div class="single-filter mb-30">
-												<h5>Instructor</h5>
-												<div class="check-box form-group mb-0">
-													<input type="checkbox" id="check8" name="check8" value="check8" checked>
-													<label for="check8">Stuard (5)</label>
-												</div>
-												<div class="check-box form-group mb-0">
-													<input type="checkbox" id="check9" name="check9" value="check9">
-													<label for="check9">Benjamin (4)</label>
-												</div>
-												<div class="check-box form-group mb-0">
-													<input type="checkbox" id="check10" name="check10" value="check10">
-													<label for="check10">Mickel (7)</label>
-												</div>
-												<div class="check-box form-group mb-0">
-													<input type="checkbox" id="check11" name="check11" value="check11">
-													<label for="check11">Johnson (2)</label>
-												</div>
-												<div class="check-box form-group mb-0">
-													<input type="checkbox" id="check12" name="check12" value="check12">
-													<label for="check12">Elijabeth (5)</label>
-												</div>
-												<div class="check-box form-group mb-0">
-													<input type="checkbox" id="check13" name="check13" value="check13">
-													<label for="check13">Rebeka (4)</label>
-												</div>
-											</div>
-
-											<div class="form-group mb-0">
-												<input class="readon2 orange" type="submit" value="Submit Now">
-											</div>
-										</form>
-									</div>
-								</div>
-							</div>
-							<div class="widget-archives mb-50">
-								<h3 class="widget-title">Course Categories</h3>
-								<ul class="categories">
-									<li><a href="#">College</a></li>
-									<li><a href="#">High School</a></li>
-									<li><a href="#">Primary</a></li>
-									<li><a href="#">School</a></li>
-									<li><a href="#">University</a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
+					<?php get_template_part("template-parts/common/sidebar") ?>
 					<div class="col-lg-8 pr-50 md-pr-15">
 						<div class="course-search-part">
 							<div class="course-view-part">
@@ -198,7 +50,7 @@ $courses_count = 0;
                                 global $product;
                             $price = $product->get_price_html();
                             $course_price = ($price > 0) ? $price : "Free";
-                            $course_categories = wc_get_product_category_list($product->get_id());
+                            $course_categories = get_the_terms($product->get_id(),'product_cat');
                                $course_url = get_permalink();
                                $course_title = get_the_title();
                                $course_image = get_the_post_thumbnail_url(get_the_ID(),'educate_course_img');
@@ -212,9 +64,17 @@ $courses_count = 0;
 								<div class="content-part">
 									<ul class="meta-part">
 										<li><span class="price"><?php echo $course_price?></span></li>
-										<li><a class="categorie" href="<?php echo esc_url($course_url)?>"><?php echo esc_html($course_title)?></a></li>
-									</ul>
-									<h3 class="title"><a href="<?php  ?>">Become a PHP Master and Make Money Fast</a></h3>
+                                        <li>
+                                            <?php if (!empty($course_categories)):
+                                                foreach ($course_categories as $course_category):
+                                                    $course_category_url = get_term_link($course_category);
+                                                ?>
+                                            <a class="categorie" href="<?php echo esc_url($course_category_url)?>"><?php echo esc_html($course_category->name)?></a>
+                                            <?php
+                                            endforeach;
+                                            endif; ?>
+                                            </li>							</ul>
+									<h3 class="title"><a href="<?php echo esc_url($course_url)?>"><?php echo esc_html($course_title)?></a></h3>
 									<div class="bottom-part">
 										<div class="info-meta">
 											<ul>
